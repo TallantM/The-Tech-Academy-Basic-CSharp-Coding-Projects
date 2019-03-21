2287a743343661d746d999d85fccc5391563978e
Michael Tallant <tall3655@gmail.com>
2019-03-21 17:40:21 -0600

    Create ClassMethodMorePractice exercise

---
+++ ClassMethodMorePractice/ClassMethodMorePractice/Integers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodMorePractice
+{
+    public class Integers
+    {
+        public void twoInt(int numberOne, int numberTwo)
+        {
+            numberOne * 2;
+            Console.WriteLine(numberTwo);
+
+        }
+    }
+}
