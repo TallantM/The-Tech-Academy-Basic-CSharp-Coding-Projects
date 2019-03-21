a6e61bba71fbec968b6f1782cac7ec02232fb305
Michael Tallant <tall3655@gmail.com>
2019-03-20 18:45:33 -0600

    Update Program.cs

--- Iteration/Iteration/Program.cs
+++ Iteration/Iteration/Program.cs
@@ -40,21 +40,59 @@ class Program
         // #6, #7, & #8
         Console.WriteLine("Search Car Make list for:");
         string search = Console.ReadLine();
+        bool match = false;
         List<string> carMake = new List<string>() { "toyota", "subaru", "honda", "nissan", "bmw" };
-        foreach(string make in carMake)
+        for (int i = 0; i < carMake.Count; i++)
         {
-            if (!make.Contains(search))
+            if (carMake[i].Contains(search))
             {
-                Console.WriteLine(make);
-                continue;
-            }
-            else
-            {
-                Console.WriteLine("Your search for " + search + " yeilded no results.");
+                Console.WriteLine(i);
+                match = true;
                 break;
             }
         }
-        
+        if (match == false)
+        {
+            Console.WriteLine("No Matching Results!");
+        }
+
+        // #9 & #10
+        Console.WriteLine("Please type a letter between a and f:");
+        string search2 = Console.ReadLine();
+        bool match2 = false;
+        List<string> alphabet = new List<string>() { "a", "b", "c", "d", "d", "e", "f" };
+        for( int i = 0; i < alphabet.Count; i++)
+        {
+            if (alphabet[i].Contains(search2))
+            {
+                Console.WriteLine(i);
+                match2 = true;
+            }
+        }
+        if (match2 == false)
+        {
+            Console.WriteLine("No Matching Results!");
+        }
+
+        // #11
+        List<string> alphabet2 = new List<string>() { "a", "b", "c", "d", "d", "e", "f" };
+        List<string> results = new List<string>();
+        foreach( string i in alphabet2)
+        {
+            if (!results.Contains(i))
+            {
+                results.Add(i);
+            }
+            else
+            {
+                results.Add(i + "dup");
+            }
+
+        }
+        foreach(string i in results)
+        {
+            Console.WriteLine(i);
+        }
 
         Console.ReadLine();
     }
