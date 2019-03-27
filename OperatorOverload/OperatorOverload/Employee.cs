3c1f0901579c5c3f6cf741e253ed3a574cbe3967
Michael Tallant <tall3655@gmail.com>
2019-03-27 15:29:13 -0600

    Create OperatorOverload practice exercise

---
+++ OperatorOverload/OperatorOverload/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    public class Employee : Person
+    {
+        public int ID { get; set; }
+    }
+
+    public static Employee operator== (Employee employee)
+    {
+        
+    }
+}
